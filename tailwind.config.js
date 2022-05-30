617a1c3ad9751c50749481cb043ac45c4f6f3a47
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-30 08:41:38 +0200

    importation couleur et fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      "colors": {
+        "Custom-red": "#e52421",
+        "Custom-black": "#191818",
+        "Custom-gray": "#a0a09f",
+        "Custom-white": "#ffffff",
+        "Custom-bordeaux": "#6e2e2b"
+      },
+      "fontFamily": {
+        "chalkduster": ["Chalkduster"],
+        "monaco": ["Monaco"]
+      },
+    },
   },
   plugins: [require('prettier-plugin-tailwindcss')],
 }
