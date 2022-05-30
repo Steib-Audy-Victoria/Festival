e08594211b02366394eb1c55ecc8e1f3f1480b0f
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-30 09:08:13 +0200

    route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProgrammationView from '../views/ProgrammationView.vue'
+import ArtistesView from '../views/ArtistesView.vue'
+import FestivalView from '../views/FestivalView.vue'
+import ContactView from '../views/ContactView.vue'
+import ConcertView from '../views/ConcertView.vue'
+import ArtisteView from '../views/ArtisteView.vue'
+import MentionsLegalesView from '../views/MentionsLegalesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +15,41 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/programmation',
+      name: 'programmation',
+      component: ProgrammationView
+    },
+    {
+      path: '/artistes',
+      name: 'artistes',
+      component: ArtistesView
+    },
+    {
+      path: '/festival',
+      name: 'festival',
+      component: FestivalView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
+    },
+    {
+      path: '/concert',
+      name: 'concert',
+      component: ConcertView
+    },
+    {
+      path: '/artiste',
+      name: 'artiste',
+      component: ArtisteView
+    },
+    {
+      path: '/mentionsLegales',
+      name: 'mentionsLegales',
+      component: MentionsLegalesView
     },
 
   ]
