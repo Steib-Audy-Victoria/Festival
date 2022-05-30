647710c20f2f1d05f56d3649943974bd590d4987
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-30 21:46:51 +0200

    Page artiste

---
+++ src/views/ArtisteView.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <div>
+      <img src="" alt="" />
+      <h2></h2>
+    </div>
+    <div>
+      <div>
+        <p></p>
+        <p></p>
+        <div>
+          icon
+          <p></p>
+          <img src="" alt="" />
+        </div>
+        <button></button>
+      </div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  </main>
+</template>
