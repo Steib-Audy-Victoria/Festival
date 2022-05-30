f6b291556ed84f22a1dc9d36d5be872efeb59736
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-30 14:26:08 +0200

    Festival

---
+++ src/views/FestivalView.vue
@@ -0,0 +1,59 @@
+<template>
+  <main class="grid grid-cols-2 place-items-center">
+    <div class="text-lg">
+      <p>
+        <span class="font-semibold">Festival de musique électronique</span>, à
+        strasbourg, du vendredi 22 juillet au dimanche 24 juillet, avec 3
+        scènes.
+      </p>
+      <p>
+        Avec des artistes de musique électronique, de techno, de Hip-Hop, de
+        électropop, de électroclash, de synthétiseur et de synthpop, de Deep
+        House, ...
+      </p>
+      <p class="font-semibold">Liste des artistes :</p>
+      <div class="grid grid-cols-2">
+        <ul class="list-disc">
+          <li>Agoria</li>
+          <li>Bob Sinclar</li>
+          <li>DJ Snake</li>
+          <li>Petit Biscuit</li>
+          <li>Fakear</li>
+          <li>Brodinski</li>
+          <li>Gesaffelstein</li>
+          <li>Madeon</li>
+          <li>Danger</li>
+          <li>The Hacker</li>
+          <li>Perturbator</li>
+          <li>Igorrr</li>
+          <li>DJ Fly</li>
+        </ul>
+        <ul class="list-disc">
+          <li>L.B. One</li>
+          <li>Marst</li>
+          <li>Max Season</li>
+          <li>Laurent Garnier</li>
+          <li>French Kiwi Juice</li>
+          <li>Koudlam</li>
+          <li>F.E.M</li>
+          <li>David Carretta</li>
+          <li>DJ Shalom, Catman</li>
+          <li>Electrosexual</li>
+          <li>Mondkopf</li>
+          <li>Panda Dub</li>
+          <li>Kiddy Smile</li>
+        </ul>
+      </div>
+      <img src="public/img/Festival1.webp" alt="image du festival" />
+      <img src="public/img/Festival2.webp" alt="image du festival" />
+    </div>
+    <div>
+      <img src="public/img/Festival_col1.webp" alt="image du festival" />
+      <img src="public/img/Festival_col2.webp" alt="image du festival" />
+      <img src="public/img/Festival_col3.webp" alt="image du festival" />
+      <img src="public/img/Festival_col4.webp" alt="image du festival" />
+      <img src="public/img/Festival_col5.webp" alt="image du festival" />
+      <img src="public/img/Festival_col6.webp" alt="image du festival" />
+    </div>
+  </main>
+</template>
