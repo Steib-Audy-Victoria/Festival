6f1b1ec484afe7eb1fa7c82c49756c8ddb956d3d
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-30 21:44:16 +0200

    page concert

---
+++ src/views/ConcertView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <div>
+      <img src="" alt="" />
+      <h2></h2>
+    </div>
+    <div>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+    <div>
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  </main>
+</template>
