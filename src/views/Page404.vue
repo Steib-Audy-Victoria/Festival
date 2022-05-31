76514420383b122eb18ebd1171367b682093ac9a
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-31 15:22:34 +0200

    design page 404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
-    <h1>Page non trouver</h1>
+    <div class="flex flex-col justify-center items-center my-20">
+      <img src="public/img/Erreur404.webp" alt="" />
+      <h1 class="text-Custom-red text-7xl font-monaco mt-6">Erreur 404</h1>
+      <p class="text-Custom-black text-xl font-monaco">Page non trouvée</p>
+    </div>
   </main>
 </template>
