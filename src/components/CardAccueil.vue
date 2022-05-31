36e6221384124213e25c35c57d8d17ef8e76b2d8
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-31 08:57:27 +0200

    correction box shadow btn accueil

--- src/components/CardAccueil.vue
+++ src/components/CardAccueil.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="rounded-2xl bg-white text-4xl text-center m-4 p-2 py-4 w-80">
+  <button
+    class="rounded-2xl bg-white text-4xl text-center m-4 p-2 py-4 w-80"
+    style="box-shadow: -3px 4px 8px 1px #6e2e2b"
+  >
     {{ Msg }}
   </button>
 </template>
