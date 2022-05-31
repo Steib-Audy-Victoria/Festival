43f1a6d576bf08ec9e2dda5231a99b303fb3f4a3
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-31 08:56:22 +0200

    correction marge page festival

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -12,60 +12,94 @@
     >
       <h2 class="text-Custom-bordeaux text-4xl">Festival</h2>
     </div>
-    <div class="text-lg grid grid-cols-2 place-items-center">
-      <p>
-        <span class="font-semibold">Festival de musique électronique</span>, à
-        strasbourg, du vendredi 22 juillet au dimanche 24 juillet, avec 3
-        scènes.
-      </p>
-      <p>
-        Avec des artistes de musique électronique, de techno, de Hip-Hop, de
-        électropop, de électroclash, de synthétiseur et de synthpop, de Deep
-        House, ...
-      </p>
-      <p class="font-semibold">Liste des artistes :</p>
-      <div class="grid grid-cols-2">
-        <ul class="list-disc">
-          <li>Agoria</li>
-          <li>Bob Sinclar</li>
-          <li>DJ Snake</li>
-          <li>Petit Biscuit</li>
-          <li>Fakear</li>
-          <li>Brodinski</li>
-          <li>Gesaffelstein</li>
-          <li>Madeon</li>
-          <li>Danger</li>
-          <li>The Hacker</li>
-          <li>Perturbator</li>
-          <li>Igorrr</li>
-          <li>DJ Fly</li>
-        </ul>
-        <ul class="list-disc">
-          <li>L.B. One</li>
-          <li>Marst</li>
-          <li>Max Season</li>
-          <li>Laurent Garnier</li>
-          <li>French Kiwi Juice</li>
-          <li>Koudlam</li>
-          <li>F.E.M</li>
-          <li>David Carretta</li>
-          <li>DJ Shalom, Catman</li>
-          <li>Electrosexual</li>
-          <li>Mondkopf</li>
-          <li>Panda Dub</li>
-          <li>Kiddy Smile</li>
-        </ul>
+    <div class="grid grid-cols-2 place-items-center">
+      <div class="text-lg ml-12">
+        <p class="mb-12">
+          <span class="font-semibold">Festival de musique électronique</span>, à
+          strasbourg, du vendredi 22 juillet au dimanche 24 juillet, avec 3
+          scènes.
+        </p>
+        <p class="mb-12">
+          Avec des artistes de musique électronique, de techno, de Hip-Hop, de
+          électropop, de électroclash, de synthétiseur et de synthpop, de Deep
+          House, ...
+        </p>
+        <p class="font-semibold mb-12">Liste des artistes :</p>
+        <div class="grid grid-cols-2 mb-12">
+          <ul class="list-disc ml-20">
+            <li>Agoria</li>
+            <li>Bob Sinclar</li>
+            <li>DJ Snake</li>
+            <li>Petit Biscuit</li>
+            <li>Fakear</li>
+            <li>Brodinski</li>
+            <li>Gesaffelstein</li>
+            <li>Madeon</li>
+            <li>Danger</li>
+            <li>The Hacker</li>
+            <li>Perturbator</li>
+            <li>Igorrr</li>
+            <li>DJ Fly</li>
+          </ul>
+          <ul class="list-disc ml-20">
+            <li>L.B. One</li>
+            <li>Marst</li>
+            <li>Max Season</li>
+            <li>Laurent Garnier</li>
+            <li>French Kiwi Juice</li>
+            <li>Koudlam</li>
+            <li>F.E.M</li>
+            <li>David Carretta</li>
+            <li>DJ Shalom, Catman</li>
+            <li>Electrosexual</li>
+            <li>Mondkopf</li>
+            <li>Panda Dub</li>
+            <li>Kiddy Smile</li>
+          </ul>
+        </div>
+        <img
+          class="my-4"
+          src="public/img/Festival1.webp"
+          alt="image du festival"
+        />
+        <img
+          class="my-4"
+          src="public/img/Festival2.webp"
+          alt="image du festival"
+        />
       </div>
-      <img src="public/img/Festival1.webp" alt="image du festival" />
-      <img src="public/img/Festival2.webp" alt="image du festival" />
-    </div>
-    <div>
-      <img src="public/img/Festival_col1.webp" alt="image du festival" />
-      <img src="public/img/Festival_col2.webp" alt="image du festival" />
-      <img src="public/img/Festival_col3.webp" alt="image du festival" />
-      <img src="public/img/Festival_col4.webp" alt="image du festival" />
-      <img src="public/img/Festival_col5.webp" alt="image du festival" />
-      <img src="public/img/Festival_col6.webp" alt="image du festival" />
+      <div>
+        <img
+          class="my-4"
+          src="public/img/Festival_col1.webp"
+          alt="image du festival"
+        />
+        <img
+          class="my-4"
+          src="public/img/Festival_col2.webp"
+          alt="image du festival"
+        />
+        <img
+          class="my-4"
+          src="public/img/Festival_col3.webp"
+          alt="image du festival"
+        />
+        <img
+          class="my-4"
+          src="public/img/Festival_col4.webp"
+          alt="image du festival"
+        />
+        <img
+          class="my-4"
+          src="public/img/Festival_col5.webp"
+          alt="image du festival"
+        />
+        <img
+          class="my-4"
+          src="public/img/Festival_col6.webp"
+          alt="image du festival"
+        />
+      </div>
     </div>
   </main>
 </template>
