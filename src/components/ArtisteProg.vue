e34e16f93d20377bb6a68010d18bf7e44b155522
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-31 08:41:04 +0200

    correction box shadow comp artisteprog

--- src/components/ArtisteProg.vue
+++ src/components/ArtisteProg.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-white drop-shadow-xl w-40 h-40 p-8 rounded-full m-2">
+  <div
+    class="bg-white drop-shadow-xl w-40 h-40 p-8 rounded-full m-2"
+    style="box-shadow: -3px 4px 8px 1px #6e2e2b"
+  >
     <div class="flex flex-col justify-center items-center">
       <img :src="image" />
       <p
