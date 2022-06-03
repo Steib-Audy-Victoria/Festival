c8054e532d9ea784680f94653367b40782a2b8f3
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-06-03 14:20:47 +0200

    modification hover artiste prog

--- src/components/ArtisteProg.vue
+++ src/components/ArtisteProg.vue
@@ -1,18 +1,22 @@
 <template>
   <div
-    class="bg-white drop-shadow-xl w-40 h-40 p-8 rounded-full m-2"
+    class="
+      bg-white
+      hover:bg-Custom-bordeaux
+      drop-shadow-xl
+      w-40
+      h-40
+      p-8
+      rounded-full
+      m-2
+      text-black
+      hover:text-white
+    "
     style="box-shadow: -3px 4px 8px 1px #6e2e2b"
   >
     <div class="flex flex-col justify-center items-center">
       <img class="rounded-full" :src="image" />
-      <p
-        class="
-          flex-grow-0 flex-shrink-0
-          text-base
-          font-semibold
-          text-center text-black
-        "
-      >
+      <p class="flex-grow-0 flex-shrink-0 text-base font-semibold text-center">
         {{ nom }}
       </p>
     </div>
